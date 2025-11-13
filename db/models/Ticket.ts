df6d4474f609c2ad25af2d47d2d5056b04d413e7
yogy fresta rahmawan <yogy.frestarahmawan@gmail.com>
2025-11-13 16:21:45 +0700

    fix category

--- db/models/Ticket.ts
+++ db/models/Ticket.ts
@@ -23,7 +23,7 @@ export enum TicketType {
 
 export enum TicketCategory {
   accounting = 'accounting',
-  corporate = 'registrationAddressChange',
+  corporate = 'corporate',
   management = 'management',
 }
 
